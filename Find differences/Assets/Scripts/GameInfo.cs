55ba455477c217c7e6df72279ef4f970353fa083
AndyLimon5010 <andreisndron@gmail.com>
2024-02-08 16:51:59 +0700

    Визуал 2
    Уменьшил значки сердца и счётчика.

--- Find differences/Assets/Scripts/GameInfo.cs
+++ Find differences/Assets/Scripts/GameInfo.cs
@@ -1,7 +1,7 @@
 
 public static class GameInfo
 {
-    public static int ActiveLevel = 1;
-    public static int LastOpenedLevel = 1;
+    public static int ActiveLevel = 17;
+    public static int LastOpenedLevel = 17;
     public static int ImagesCount = 20;
 }
